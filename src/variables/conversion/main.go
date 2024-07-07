51de7c5c87be42ea70aa92e7ed801ef415e81df6
caiquemiranda <caiquemirandas@gmail.com>
2024-07-07 15:31:56 -0300

    variables

---
+++ src/variables/conversion/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var a int = 42
+var b string = "James Bond"
+
+func main() {
+	var c float64 = float64(a)
+	fmt.Println(c, b)
+}
---
+++ src/variables/data_type/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var a = 42
+var b = "James Bond"
+
+func main() {
+	fmt.Printf("O tipo da variavel A é: %T e B é %T", a, b)
+}
