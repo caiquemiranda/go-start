fce9d11166aa9ccc36eb2d383e2db5a2f1e71df2
caiquemiranda <caiquemirandas@gmail.com>
2024-07-16 22:42:45 -0300

    feat: package errors

--- src/packages/error/main.go
+++ src/packages/error/main.go
@@ -1,5 +1,27 @@
 package main
 
 import (
-	"errors"
+	"fmt"
+	"time"
 )
+
+type MyError struct {
+	When time.Time
+	What string
+}
+
+func (e MyError) Error() string {
+	return fmt.Sprintf("%v: %v", e.When, e.What)
+
+}
+func oops() error {
+	return MyError{
+		time.Date(19989, 3, 15, 22, 30, 0, 0, time.UTC),
+		"O arquivo do sistema desapareceu!",
+	}
+}
+func main() {
+	if err := oops(); err != nil {
+		fmt.Println(err)
+	}
+}
